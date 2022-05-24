c21135cb62a334df0dffe73e001a46f257cdef80
tobyatgithub <toby.fangyuan@gmail.com>
2022-05-24 16:56:47 -0700

    hop over mouse to show the delete button, and hide delete button once mouse leave.

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Item({ task }) {
+  const [shallDisplay, setShallDisplay] = useState("none");
+
+  const handleCheckbox = () => {
+    task.done = !task.done;
+    console.log("clicked!", task);
+  };
+
+  const handleFocus = () => {
+    setShallDisplay("inline");
+  };
+
+  const handleBlur = () => {
+    setShallDisplay("none");
+  };
+
   return (
-    <li>
+    <li onMouseOver={handleFocus} onMouseLeave={handleBlur}>
       <label>
-        <input type="checkbox" defaultChecked={task.done}></input>
+        <input
+          type="checkbox"
+          defaultChecked={task.done}
+          onChange={handleCheckbox}
+        ></input>
         <span>{task.text}</span>
       </label>
-      <button className="btn btn-danger" style={{ display: "none" }}>
+      <button className="btn btn-danger" style={{ display: shallDisplay }}>
         Delete
       </button>
     </li>
