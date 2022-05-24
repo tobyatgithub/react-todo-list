61c583f7e93d4da262900bb10354a63041483053
tobyatgithub <toby.fangyuan@gmail.com>
2022-05-24 16:46:26 -0700

    reset the search box after enter.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 
 export default function Header({ taskList, setTaskList }) {
   const handleSubmit = (event) => {
+    // if ENTER clicked
     if (event.keyCode === 13) {
       const newTaskList = taskList.concat({
-        id: 2,
+        id: 2, //TODO: hmm need to fix id collision
         text: event.target.value,
         done: false,
       });
       setTaskList(newTaskList);
+      // reset the search box
+      event.target.value = "";
     }
   };
+
   return (
     <div className="todo-header">
       <input
